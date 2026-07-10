d43e6ec78c54e0b80be190107ce497db2bc43672
deepanshuc7 <c7deepanshu@gmail.com>
2026-07-10 19:15:52 +0200

    feat: add typing state model

---
+++ frontend/src/types/typing.ts
@@ -0,0 +1,18 @@
+export type TestStatus = 
+    | "idle"
+    | "running"
+    | "finished";
+
+    export interface TypingState {
+        status: TestStatus;
+
+        words: string[];
+
+        typedText: string;
+
+        currentWordIndex: number;
+
+        currentCharacterIndex: number;
+
+        mistakes: number;
+    }
